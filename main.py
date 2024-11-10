8c3d6caffabb46503e4a44ee351af09873b41328
Mike Rouse <mikerouse831@gmail.com>
2024-11-10 15:34:24 +0000

    basic playground ui

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import json
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 # Load social networks data from JSON file
 with open('social_networks.json', 'r') as file:
     social_networks = json.load(file)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/api/social-networks', methods=['GET'])
 def get_social_networks():
